605d3b438c9e2c14964c89edb93bf88d744c9172
ZM-0 <zareef.m@hotmail.com>
2024-01-15 09:27:58 +1100

    Add API route for getting list of tasks

--- output/server.js
+++ output/server.js
@@ -1,5 +1,43 @@
 import express from "express";
+import path from "node:path";
+import url from "node:url";
+import pg from "pg";
+const appliation = express();
 const HOST = "localhost";
 const PORT = 8000;
-const appliation = express();
+const directory = path.dirname(url.fileURLToPath(import.meta.url));
+// ====================================================================================================
+// Database Setup
+// Must set environment variables PGUSER and PGPASSWORD to Postgres username and password
+const client = new pg.Client({
+    host: "localhost",
+    port: 5432,
+    database: "tododata"
+});
+await client.connect();
+// try {
+//     const response: pg.QueryResult = await client.query('SELECT * FROM "TASK"');
+//     console.log(response.rows);
+// } catch (error) {
+//     console.error(error);
+// } finally {
+//     await client.end();
+// }
+// ====================================================================================================
+// Middleware
+appliation.use((request, response, next) => {
+    console.log(`Received ${request.method} request for ${request.path}`);
+    next();
+});
+// ====================================================================================================
+// API
+// Gets the list of tasks
+appliation.get("/api/tasks", (request, response) => {
+    client
+        .query('SELECT * FROM "TASK"')
+        .then((queryResult) => {
+        response.json(queryResult.rows);
+    })
+        .catch(console.error);
+});
 appliation.listen(PORT, HOST, () => console.log(`Server listening at ${HOST}:${PORT}...`));
--- source/server.ts
+++ source/server.ts
@@ -1,7 +1,58 @@
 import express from "express";
+import path from "node:path";
+import url from "node:url";
+import pg from "pg"
 
+
+const appliation: express.Application = express();
 const HOST: string = "localhost";
 const PORT: number = 8000;
-const appliation: express.Application = express();
+const directory: string = path.dirname(url.fileURLToPath(import.meta.url));
+
+
+// ====================================================================================================
+// Database Setup
+
+// Must set environment variables PGUSER and PGPASSWORD to Postgres username and password
+const client: pg.Client = new pg.Client({
+    host: "localhost",
+    port: 5432,
+    database: "tododata"
+});
+
+await client.connect();
+
+// try {
+//     const response: pg.QueryResult = await client.query('SELECT * FROM "TASK"');
+//     console.log(response.rows);
+// } catch (error) {
+//     console.error(error);
+// } finally {
+//     await client.end();
+// }
+
+
+// ====================================================================================================
+// Middleware
+
+appliation.use((request: express.Request, response: express.Response, next: express.NextFunction): void => {
+    console.log(`Received ${request.method} request for ${request.path}`);
+    next();
+});
+
+
+// ====================================================================================================
+// API
+
+// Gets the list of tasks
+appliation.get("/api/tasks", (request: express.Request, response: express.Response): void => {
+    client
+    .query('SELECT * FROM "TASK"')
+    .then((queryResult: pg.QueryResult): void => {
+        response.json(queryResult.rows);
+    })
+    .catch(console.error);
+});
+
 
 appliation.listen(PORT, HOST, () => console.log(`Server listening at ${HOST}:${PORT}...`));
